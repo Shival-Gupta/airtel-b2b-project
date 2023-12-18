421cc04459aca8a573e3e75936647d6834f8c21d
Shival Gupta <sgupta.5545@gmail.com>
2023-12-18 06:45:52 +0530

    Update: 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,20 @@
-import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "@/components/ui/select"
-import Link from "next/link"
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
-export default function NotFound() {
-    return (
-        <div>
-            <h2>Not Found</h2>
-            <p>Could not find requested Page</p>
-            <Link href="/">Return Dashboard</Link>
-        </div>
-    )
-}
+const NotFoundPage = () => {
+  return (
+    <div className="flex items-center justify-center my-36">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4">404 - Not Found</h1>
+        <p className="text-primary mb-4">Oops! The page you are looking for does not exist.</p>
+        <Button asChild>
+          <Link href="/">
+            Go back home
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
