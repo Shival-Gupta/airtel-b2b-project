96f7075dce4df71628737a41a9a6b0a0efb76131
Shival Gupta <sgupta.5545@gmail.com>
2023-12-18 17:37:58 +0530

    update: footer

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,7 +4,7 @@ import { orgUrl } from "../routeData";
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex p-24 flex-col items-center justify-center">
       <Link href={orgUrl}>
         <div className="mb-4 flex items-center border shadow-sm p-4 bg-primary text-primary-foreground rounded-full uppercase hover:bg-destructive">
           <Medal className="h-6 w-6 mr-2" />
--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,47 +1,58 @@
-"use client"
+"use client";
 
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const aboutUrl = "https://shival-gupta.github.io/";
 
 export const Footer = () => {
   return (
-    <footer className="bottom-0 w-full p-8 px-10 lg:px-32 border-t shadow-2xl bg-background flex items-center">
-      <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-center">
+    <footer className="bottom-0 w-full border-t shadow-2xl bg-background flex-row items-center">
+      <div className="md:max-w-screen-2xl p-8 px-10 lg:px-32 mx-auto flex items-center w-full justify-center">
         <div className="flex flex-col md:flex-row items-center md:items-start md:justify-start justify-center gap-12 text-sm">
 
           <div className="flex flex-col items-center">
             <p>Quick Access</p>
-            <Button variant={"disabled"} className="text-xs">Retail Banking</Button>
-            <Button variant={"disabled"} className="text-xs">Investor Relation</Button>
-            <Button variant={"disabled"} className="text-xs">Terms and Condition</Button>
-            <Button variant={"disabled"} className="text-xs">About Us</Button>
+            <Button variant={"disabled"} className="text-xs" asChild>
+              <Link href={aboutUrl} target='_blank'>About Us</Link>
+            </Button>
+            <Button variant={"disabled"} className="text-xs" asChild>
+              <Link href={aboutUrl} target='_blank'>Retail Banking</Link>
+            </Button>
+            <Button variant={"disabled"} className="text-xs" asChild>
+              <Link href={aboutUrl} target='_blank'>Investor Relation</Link>
+            </Button>
+            <Button variant={"disabled"} className="text-xs" asChild>
+              <Link href={aboutUrl} target='_blank'>Terms and Condition</Link>
+            </Button>
           </div>
 
           <div className="flex flex-col items-center">
             <p>Help at Hand</p>
-            <Button variant={"disabled"} className="text-xs">
-              Banking Points
+            <Button variant={"disabled"} className="text-xs" asChild>
+              <Link href={aboutUrl} target='_blank'>Banking Points</Link>
             </Button>
-            <Button variant={"disabled"} className="text-xs">
-              Banking Security Tips
+            <Button variant={"disabled"} className="text-xs" asChild>
+              <Link href={aboutUrl} target='_blank'>Banking Security Tips</Link>
             </Button>
-            <Button variant={"disabled"} className="text-xs">
-              Fraud Awareness Scenarios
+            <Button variant={"disabled"} className="text-xs" asChild>
+              <Link href={aboutUrl} target='_blank'>Fraud Awareness Scenarios</Link>
             </Button>
-            <Button variant={"disabled"} className="text-xs">
-              FAQs
+            <Button variant={"disabled"} className="text-xs" asChild>
+              <Link href={aboutUrl} target='_blank'>FAQs</Link>
             </Button>
           </div>
 
           <div className="flex flex-col items-center">
             <p>Social Connect</p>
-            <Button variant={"disabled"} className="text-xs">
-              Facebook
+            <Button variant={"disabled"} className="text-xs" asChild>
+              <Link href={aboutUrl} target='_blank'>Facebook</Link>
             </Button>
-            <Button variant={"disabled"} className="text-xs">
-              Twitter
+            <Button variant={"disabled"} className="text-xs" asChild>
+              <Link href={aboutUrl} target='_blank'>Twitter</Link>
             </Button>
-            <Button variant={"disabled"} className="text-xs">
-              LinkedIn
+            <Button variant={"disabled"} className="text-xs" asChild>
+              <Link href={aboutUrl} target='_blank'>LinkedIn</Link>
             </Button>
           </div>
 
@@ -56,6 +67,14 @@ export const Footer = () => {
           </div>
 
         </div>
+      </div>
+      <div className="flex py-2 text-sm bg-accent text-accent-foreground justify-center items-center">
+        <p>
+          Designed & Built by{" "}
+          <Link href={aboutUrl} target='_blank' className="font-semibold text-primary hover:underline">
+            Shival Gupta
+          </Link>
+        </p>
       </div>
     </footer>
   );
